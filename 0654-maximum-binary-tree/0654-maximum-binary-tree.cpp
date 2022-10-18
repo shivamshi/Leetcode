5b63ec145b28aee55ebc7bb580e149ebedbc3545
shivam <67598248+shivamshi@users.noreply.github.com>
2022-10-18 08:19:50 +0530

    Time: 2095 ms (5.01%), Space: 379.1 MB (8.21%) - LeetHub

---
+++ 0654-maximum-binary-tree/0654-maximum-binary-tree.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    TreeNode *func(int l,int r,priority_queue<pair<int,int>>q){
+        if(l>r)
+            return NULL;
+        while(q.size() &&(q.top().second<l || q.top().second>r))
+            q.pop();
+        TreeNode *temp=new TreeNode(q.top().first);
+        int t=q.top().second;
+        q.pop();
+        temp->left=func(l,t-1,q);
+        temp->right=func(t+1,r,q);
+        return temp;
+    }
+    TreeNode* constructMaximumBinaryTree(vector<int>& nums) {
+        priority_queue<pair<int,int>>q;
+        for(int i=0;i<nums.size(); i++){
+            q.push({nums[i],i});
+        }
+        TreeNode * ans=new TreeNode(q.top().first);
+        int t=q.top().second;
+        q.pop();
+        ans->left=func(0,t-1,q);
+        ans->right=func(t+1,nums.size()-1,q);
+        return ans;
+    }
+    
+};
