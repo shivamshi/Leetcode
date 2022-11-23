c09e7fff119f21d7b81c0b44829f7a4f77be9842
shivam <67598248+shivamshi@users.noreply.github.com>
2022-11-23 11:57:38 +0530

    Time: 898 ms (38.09%), Space: 5.9 MB (94.52%) - LeetHub

---
+++ 2002-maximum-product-of-the-length-of-two-palindromic-subsequences/2002-maximum-product-of-the-length-of-two-palindromic-subsequences.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+long long ans = 0;
+    
+    bool isPal(string &s)
+    {
+        int start=0, end = s.length()-1;
+        while(start < end)
+        {
+            if(s[start] != s[end]) return false;
+            start++; end--;
+        }
+        return true;
+    }
+    
+    void recur(string &s,int idx,string &s1,string &s2)
+    {
+        if(idx == s.size())
+        {
+            if(isPal(s1) and isPal(s2))
+            {
+                long long val =(int) s1.length() * s2.length();
+                ans = max(ans , val);
+            }
+            return;
+        }
+        
+        s1.push_back(s[idx]);
+        recur(s,idx+1,s1, s2);
+        s1.pop_back();
+        
+        s2.push_back(s[idx]);
+        recur(s,idx+1,s1,s2);
+        s2.pop_back();
+        
+        recur(s,idx+1,s1,s2);
+    }
+	
+	int maxProduct(string s) {
+        string s1="", s2 = "";
+        recur(s,0,s1,s2);
+        return ans;
+    }
+};
